0e554106bdfd9b4054869600148911e323275c51
Alekseeyy <87644820+Alekseeyy@users.noreply.github.com>
2021-11-05 19:53:58 +0300

    Update ShopUtils.java

--- src/main/java/ShopUtils.java
+++ src/main/java/ShopUtils.java
@@ -6,12 +6,12 @@ public class ShopUtils {
 
     private static Map<String, Product> getProductsMap() {
         productsMap = new HashMap<>();
-        productsMap.put("0", new Food("Гречка", 200, "10.01.2022"));
-        productsMap.put("1", new Food("Йогурт", 100, "10.10.2021"));
-        productsMap.put("2", new Clothes("Кофта", 1000, 50));
-        productsMap.put("3", new Clothes("Носки", 100, 43));
-        productsMap.put("4", new HouseholdChemicals("Мыло", 150, false));
-        productsMap.put("5", new HouseholdChemicals("Зубная паста", 200, false));
+        productsMap.put("0", new Food("Гречка", 600, "10.01.2022"));
+        productsMap.put("1", new Food("Йогурт", 400, "10.11.2021"));
+        productsMap.put("2", new Clothes("Кофта", 3000, 50));
+        productsMap.put("3", new Clothes("Носки", 500, 43));
+        productsMap.put("4", new HouseholdChemicals("Мыло", 700, false));
+        productsMap.put("5", new HouseholdChemicals("Зубная паста", 500, false));
 
         return productsMap;
     }
